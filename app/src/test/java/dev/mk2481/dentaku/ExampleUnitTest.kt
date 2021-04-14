2683cb7f7d747c8a903be1f600b2eda6b1db2151
KoutaMatsushita <kouta19960921+github@gmail.com>
2021-04-14 21:40:02 +0900

    junit5 の整備

--- app/src/test/java/dev/mk2481/dentaku/ExampleUnitTest.kt
+++ app/src/test/java/dev/mk2481/dentaku/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package dev.mk2481.dentaku
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
